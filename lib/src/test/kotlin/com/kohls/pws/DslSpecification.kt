e1d9232bf727b5b40a0ba1e821464651748e3043
Christopher Roberts <christopher.roberts@kohls.com>
2023-10-30 11:26:12 -0400

    first validation structure

--- lib/src/test/kotlin/com/kohls/pws/DslSpecification.kt
+++ lib/src/test/kotlin/com/kohls/pws/DslSpecification.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import java.io.File
+import kotlin.time.Duration.Companion.seconds
 
 class DslSpecification : StringSpec({
     "verify structure" {
@@ -22,6 +23,12 @@ class DslSpecification : StringSpec({
 
             project("OLM Stub Server") {
                 source = local("~/projects/olm-meta-repo/olm-stubs")
+                task<ShellScript> {
+                    command = "mvn clean install exec:java"
+                    validations += log(duration = 30.seconds) {
+                        contains("StubServer is running.")
+                    }
+                }
             }
             project("OLM Gateway Service") {
                 source = git(url = "git@gitlab.com:kohls/scps/scf/olm/olm-gateway.git", branch = "main", directory = "/tmp/workspace/olm-gateway")
@@ -40,12 +47,19 @@ class DslSpecification : StringSpec({
         } shouldBe Workspace(
             Directory("/tmp/hello"), mutableSetOf(
                 Project(
+                    "OLM Shared Utilities",
+                    source = Source.Git(url = "git@gitlab.com:kohls/scps/scf/olm/olm-shared-utils.git", branch = "main", directory = Directory("/tmp/workspace/olm-shared-utils")),
+                ),
+                Project(
                     "OLM Configuration Service",
                     executesAfterList = listOf("OLM Shared Utilities"),
                     source = Source.Local(Directory("~/projects/olm-meta-repo/config-server")),
                 ),
                 Project(
-                    "OLM Stub Server", source = Source.Local(Directory("~/projects/olm-meta-repo/olm-stubs"))
+                    name = "OLM Stub Server",
+                    source = Source.Local(Directory("~/projects/olm-meta-repo/olm-stubs")),
+                    tasks = listOf(ShellScript(command = "mvn clean install exec:java", validations = listOf(Validation.Log(duration = 30.seconds, contains = listOf("StubServer is running.")))))
+
                 ),
                 Project(
                     "OLM Gateway Service",
@@ -58,10 +72,6 @@ class DslSpecification : StringSpec({
                     executesAfterList = listOf("OLM Shared Utilities", "OLM Configuration Service"),
                     source = Source.Git(url = "git@gitlab.com:kohls/scps/scf/olm/order-create.git", branch = "main", directory = Directory("/tmp/workspace/order-create")),
                     canBeParallelized = true
-                ),
-                Project(
-                    "OLM Shared Utilities",
-                    source = Source.Git(url = "git@gitlab.com:kohls/scps/scf/olm/olm-shared-utils.git", branch = "main", directory = Directory("/tmp/workspace/olm-shared-utils")),
                 ),
             )
         )
@@ -128,18 +138,29 @@ data class Workspace(val targetDirectory: Directory, val projects: Set<Project>)
     }
 }
 
-data class Project(val name: String, val executesAfterList: List<String> = listOf(), val source: Source = Source.UNKNOWN, val canBeParallelized: Boolean = false) {
+data class Project(
+    val name: String, val executesAfterList: List<String> = listOf(), val source: Source = Source.UNKNOWN, val tasks: List<Task> = listOf(), val canBeParallelized: Boolean = false
+) {
     class Builder {
         lateinit var name: String
-        private var executesAfterList: List<String> = mutableListOf()
+        private val executesAfterList: MutableList<String> = mutableListOf()
         var source: Source = Source.UNKNOWN
         var canBeParallelized: Boolean = false
+        val tasks: MutableList<Task> = mutableListOf()
         fun build(): Project {
-            return Project(name = name, executesAfterList = executesAfterList, source = source, canBeParallelized = canBeParallelized)
+            return Project(
+                name = name, executesAfterList = executesAfterList, source = source, canBeParallelized = canBeParallelized, tasks = tasks
+            )
         }
 
         fun executesAfter(name: String) {
             executesAfterList += name
+        }
+
+        inline fun <reified T : Task> task(noinline block: T.() -> Unit) {
+            val taskInstance = T::class.java.getDeclaredConstructor().newInstance()
+            taskInstance.block()
+            tasks += taskInstance
         }
 
         fun local(path: String): Source = Source.Local(Directory(path))
@@ -192,3 +213,35 @@ data class Runbook(val executionOrder: List<Step>) {
 data class Step(val project: Project)
 
 
+interface Validation {
+    data class Log(val duration: kotlin.time.Duration, val contains: List<String> = mutableListOf()) : Validation {
+        class Builder {
+            var duration: kotlin.time.Duration = 30.seconds
+            val contains: MutableList<String> = mutableListOf()
+            fun build(): Log {
+                return Log(duration = duration, contains = contains)
+            }
+
+            fun contains(text: String) {
+                this.contains += text
+            }
+
+        }
+    }
+}
+
+
+interface Task {
+    var validations: List<Validation>
+}
+
+data class ShellScript(var command: String? = null, override var validations: List<Validation> = emptyList()) : Task {
+    constructor() : this(null, emptyList())
+}
+
+fun log(duration: kotlin.time.Duration, block: Validation.Log.Builder.() -> Unit): Validation.Log {
+    val builder = Validation.Log.Builder()
+    builder.block()
+    builder.duration = duration
+    return builder.build()
+}
