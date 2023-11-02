da989b1e6340bb9bc2e8bee0b07eebe83317107c
Christopher Roberts <christopher.roberts@kohls.com>
2023-11-02 09:48:39 -0400

    maven compiler updates

--- lib/src/test/kotlin/com/kohls/pws/v2/tasks/MavenCompilerTest.kt
+++ lib/src/test/kotlin/com/kohls/pws/v2/tasks/MavenCompilerTest.kt
@@ -6,9 +6,10 @@ import java.io.File
 
 class MavenCompilerTest : StringSpec({
 
-    val original = Maven(args = listOf(), variables = mutableMapOf(), background = false, settingsXmlFilePath = File("/tmp/.m2/settings.xml"), pomXmlFilePath = File("/tmp/pom.xml"), validations = listOf())
+    val original = Maven(args = listOf(), variables = mutableMapOf(), background = false, settingsXmlFilePath = File("/tmp/settings.xml"), pomXmlFilePath = File("/tmp/pom.xml"), validations = listOf())
 
-    "Missing pom is derived from project source" { MavenCompiler().compile(original.copy(pomXmlFilePath = null)) shouldBe original }
+    "Missing pom is derived from project source" { MavenCompiler().compile(original.copy(settingsXmlFilePath = null)) shouldBe original }
+    "Missing settings is derived from project source" { MavenCompiler().compile(original.copy(pomXmlFilePath = null)) shouldBe original }
 
     "Nothing happens when all fields are populated" { MavenCompiler().compile(original) shouldBe original }
 })
@@ -17,6 +18,7 @@ class MavenCompiler {
     val lookupTable = LookupTable()
     fun compile(maven: Maven): Maven {
         maven.pomXmlFilePath = maven.pomXmlFilePath ?: File("${lookupTable.findProjectSourcePath()}/pom.xml")
+        maven.settingsXmlFilePath = maven.settingsXmlFilePath ?: File("${lookupTable.findProjectSourcePath()}/settings.xml")
         return maven
     }
 
