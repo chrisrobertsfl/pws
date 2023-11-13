d64c0ac80bd38e4df9c56937055ec6b87290ab52
Christopher Roberts <chrisrobertsfl@gmail.com>
2023-11-13 07:54:48 -0500

    git checkout refactored

--- lib/src/main/kotlin/com/kohls/pws/GitCheckout.kt
+++ lib/src/main/kotlin/com/kohls/pws/GitCheckout.kt
@@ -3,9 +3,8 @@ package com.kohls.pws
 import com.kohls.base.Directory
 import org.slf4j.LoggerFactory
 
-
 // TODO: Unit test!
-data class GitCheckout(override val name: String = generateName(), var branchName: String = "main") : Action {
+data class GitCheckout(override val name: String = generateName(), var branchName: String = BRANCH) : Action {
     var targetDirectoryPath: String? = null
     private val logger by lazy { LoggerFactory.getLogger(GitCheckout::class.java) }
     override fun perform(parameters: Parameters): Parameters {
@@ -16,18 +15,18 @@ data class GitCheckout(override val name: String = generateName(), var branchNam
         return Parameters.create("targetDirectory" to targetDirectory)
     }
 
-    private fun resolveTargetDirectoryPath(parameters: Parameters) = if (targetDirectoryPath != null) {
-        require(Directory(targetDirectoryPath!!).exists()) { "Invalid targetDirectoryPath" }
-        targetDirectoryPath!!
-    } else {
-        requireNotNull(parameters["targetDirectory"]) { "Missing targetDirectoryPath" }.let {
-            val directory = it as Directory
-            directory.path
+    private fun resolveTargetDirectoryPath(parameters: Parameters): String {
+        targetDirectoryPath?.let {
+            val directory = Directory(it)
+            require(directory.exists()) { "Invalid targetDirectoryPath: $it does not exist." }
+            return it
         }
+        val targetDirectory = requireNotNull(parameters.get<Directory>("targetDirectory")) { "Missing targetDirectoryPath" }
+        return targetDirectory.path
     }
 
     companion object {
         private val BASH_SCRIPT = BashScript(commandName = "git-checkout", body = Body.fromResource("/bash-scripts/git-checkout.sh"))
+        private const val BRANCH = "main"
     }
-
 }
