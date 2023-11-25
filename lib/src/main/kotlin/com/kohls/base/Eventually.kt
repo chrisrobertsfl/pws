7d3137e62aad586b4ea6c546ed12da1becdd4d31
Christopher Roberts <chrisrobertsfl@gmail.com>
2023-11-25 11:14:44 -0500

    pulled out

--- lib/src/main/kotlin/com/kohls/base/Eventually.kt
+++ lib/src/main/kotlin/com/kohls/base/Eventually.kt
@@ -19,24 +19,6 @@ sealed interface CriteriaMet {
     }
 }
 
-data class NumberOfAttempts(val attempts: Int, val initialDelay: Duration = INITIAL_DELAY, val interval: Duration = INTERVAL) : CriteriaMet {
-    private val logger by lazy { LoggerFactory.getLogger(NumberOfAttempts::class.java) }
-
-    override fun checkCondition(condition: Condition): Boolean {
-        logger.debug("$initialDelay initial delay")
-        var numberOfAttempts: Int = 1
-        delayFor(initialDelay)
-        repeat(attempts) {
-            logger.debug("$numberOfAttempts of $attempts:  Checking condition ${condition.name ?: ""}")
-            if (condition.isMet()) return true
-            numberOfAttempts += 1
-            logger.debug("$interval interval delay")
-            delayFor(interval)
-        }
-        return false
-    }
-}
-
 data class TimeFrame(val duration: Duration, val initialDelay: Duration = INITIAL_DELAY, val interval: Duration = INTERVAL) : CriteriaMet {
 
     private val logger by lazy { LoggerFactory.getLogger(NumberOfAttempts::class.java) }
---
+++ lib/src/main/kotlin/com/kohls/base/NumberOfAttempts.kt
@@ -0,0 +1,22 @@
+package com.kohls.base
+
+import org.slf4j.LoggerFactory
+import kotlin.time.Duration
+
+data class NumberOfAttempts(val attempts: Int, val initialDelay: Duration = CriteriaMet.INITIAL_DELAY, val interval: Duration = CriteriaMet.INTERVAL) : CriteriaMet {
+    private val logger by lazy { LoggerFactory.getLogger(NumberOfAttempts::class.java) }
+
+    override fun checkCondition(condition: Condition): Boolean {
+        logger.debug("$initialDelay initial delay")
+        var numberOfAttempts: Int = 1
+        delayFor(initialDelay)
+        repeat(attempts) {
+            logger.debug("$numberOfAttempts of $attempts:  Checking condition ${condition.name ?: ""}")
+            if (condition.isMet()) return true
+            numberOfAttempts += 1
+            logger.debug("$interval interval delay")
+            delayFor(interval)
+        }
+        return false
+    }
+}
