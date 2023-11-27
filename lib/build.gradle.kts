9089785786bb8a480ce771dd7c26ba708d9589e3
Christopher Roberts <chrisrobertsfl@gmail.com>
2023-11-27 11:45:23 -0500

    Added new way of registering Actions

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.4.11")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.21")
 
     implementation("org.danilopianini:khttp:1.4.1")
     implementation("com.ingenifi:infera:1.0.0")
